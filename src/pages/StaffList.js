2820c3cfebc4baa1e52fbdd1eae35968c36dc154
“derrick” <derrickmbugua50@gmail.com>
2022-11-18 17:28:36 +0300

    feat: display list

--- src/pages/StaffList.js
+++ src/pages/StaffList.js
@@ -1,8 +1,26 @@
+const DummyData = [
+  {
+    id: '1',
+    name: 'Derrick Mwema',
+    department: 'Deep State',
+  },
+  {
+    id: '2',
+    name: 'Dextero Mbugua',
+    department: 'Mkenyadamu',
+  },
+];
+
 function StaffListPage() {
 
   return (
     <div>
       <h1>All Staff</h1>
+      <ul>
+        {DummyData.map((staff) => {
+          return <li key={staff.id}>{staff.name}</li>
+        })}
+      </ul>
     </div>
   );
 
